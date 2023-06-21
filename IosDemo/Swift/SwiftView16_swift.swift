1a4f2c495a3864e804e7e17093c5a64014400423
webabcd <webabcd@hotmail.com>
2023-06-21 15:39:36 +0800

    update

--- IosDemo/Swift/SwiftView16_swift.swift
+++ IosDemo/Swift/SwiftView16_swift.swift
@@ -3,7 +3,7 @@
  *
  *
  * 如果需要 swift 调用 oc
- * 1、需要创建一个名为 项目名称-Bridging-Header.h 的文件（桥接文件），并在 Build Settings 的 Objective-C Bridging Header 中配置好此文件的路径
+ * 1、需要创建一个名为 项目名称-Bridging-Header.h 的文件（桥接文件），并在 Build Settings 的 Objective-C Bridging Header 中配置好此文件的路径（注：在 oc 项目中新建一个 swift 文件时，xcode 会提示是否需要自动创建桥接文件）
  *    a) 本例中会有一个 IosDemo-Bridging-Header.h 文件，Objective-C Bridging Header 配置的值是 IosDemo/Swift/IosDemo-Bridging-Header.h
  *    b) 如果是在 swift 项目中创建 oc 文件，则会有弹窗提示是否需要创建此文件，如果确认的话则 xcode 会自动完成上述工作
  * 2、在 项目名称-Bridging-Header.h 的文件中通过 import 导入你需要调用的 .h 文件
