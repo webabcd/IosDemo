4bd7eb6cd7277479ae097cb3a5989b4a92b546ff
webabcd <webabcd@hotmail.com>
2023-06-29 15:52:02 +0800

    update

--- IosDemo/Swift/SwiftView16_oc.m
+++ IosDemo/Swift/SwiftView16_oc.m
@@ -4,12 +4,16 @@
  *
  * 如果需要 oc 调用 swift
  * 1、需要桥接文件（此文件是不可见，但是可引用的）
- *    a) 对于 swift 项目来说，此文件在你新建项目的时候就自动生成了
+ *    a) 对于 swift 项目来说，此文件在你新建项目的时候就自动生成了，在 Build Settings 中的 Objective-C Generated Interface Header Name 配置的值是 $(SWIFT_MODULE_NAME)-Swift.h
  *    b) 如果是在 oc 项目中创建 swift 文件，则会有弹窗提示是否需要创建此文件，如果确认的话则 xcode 会自动完成上述工作
  * 2、引用桥接文件
  *    a) 桥接文件的文件名为 项目名称-Swift.h
-*     b) 本例中此桥接文件的文件名为 IosDemo-Swift.h，通过 #import "IosDemo-Swift.h" 引用即可
+ *    b) 本例中此桥接文件的文件名为 IosDemo-Swift.h，通过 #import "IosDemo-Swift.h" 引用即可
  * 3、然后你就可以按 oc 的方式调用 swift 了
+ *
+ *
+ * 注：
+ * 如果你的 Build Settings 中没有 Objective-C Generated Interface Header Name 配置的话则可以文本方式编辑 project.pbxproj 文件，并在相关的位置增加 SWIFT_OBJC_INTERFACE_HEADER_NAME = "$(SWIFT_MODULE_NAME)-Swift.h";
  */
 
 #import "SwiftView16_oc.h"
--- IosDemo/Swift/SwiftView16_swift.swift
+++ IosDemo/Swift/SwiftView16_swift.swift
@@ -3,8 +3,8 @@
  *
  *
  * 如果需要 swift 调用 oc
- * 1、需要创建一个名为 项目名称-Bridging-Header.h 的文件（桥接文件），并在 Build Settings 的 Objective-C Bridging Header 中配置好此文件的路径（注：在 oc 项目中新建一个 swift 文件时，xcode 会提示是否需要自动创建桥接文件）
- *    a) 本例中会有一个 IosDemo-Bridging-Header.h 文件，Objective-C Bridging Header 配置的值是 IosDemo/Swift/IosDemo-Bridging-Header.h
+ * 1、需要创建一个名为 项目名称-Bridging-Header.h 的文件（桥接文件），并在 Build Settings 的 Objective-C Bridging Header 中配置好此文件的路径
+ *    a) 本例中会有一个 IosDemo-Bridging-Header.h 文件，在 Build Settings 中的 Objective-C Bridging Header 配置的值是 IosDemo/Swift/IosDemo-Bridging-Header.h
  *    b) 如果是在 swift 项目中创建 oc 文件，则会有弹窗提示是否需要创建此文件，如果确认的话则 xcode 会自动完成上述工作
  * 2、在 项目名称-Bridging-Header.h 的文件中通过 import 导入你需要调用的 .h 文件
  *    a) 本例中的 IosDemo-Bridging-Header.h 文件的内容为 # import "SwiftView16_oc.h"
@@ -14,6 +14,10 @@
  * 如果需要 swift 被 oc 调用
  * 1、swift 的类要继承 NSObject
  * 2、允许被 oc 调用的 swift 的方法要用 @objc 标记
+ *
+ *
+ * 注：
+ * 如果你的 Build Settings 中没有 Objective-C Bridging Header 配置的话则可以文本方式编辑 project.pbxproj 文件，并在相关的位置增加 SWIFT_OBJC_BRIDGING_HEADER = "IosDemo/Swift/IosDemo-Bridging-Header.h";
  */
 
 import Foundation
